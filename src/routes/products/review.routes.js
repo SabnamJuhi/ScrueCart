0890a51b3da369c894f97958f5135829cc6691db
SabnamJuhi <sjuhisarkars95@gmail.com>
2026-02-18 10:16:33 +0530

    added like route

--- src/routes/products/review.routes.js
+++ src/routes/products/review.routes.js
@@ -3,11 +3,13 @@ const express = require("express")
 const router = express.Router()
 const {protected} = require("../../middleware/user.logout.middleware")
 const reviewController = require("../../controllers/products/productReview.controller")
+const { reactToReview } = require("../../controllers/products/reviewReaction.controller")
 
 // Reviews
 router.post("/", protected, reviewController.createReview)
 router.get("/:productId", reviewController.getReviewsByProduct)
 router.put("/:id", protected, reviewController.updateReview)
 router.delete("/:id", protected, reviewController.deleteReview)
+router.post("/:id/react", protected, reactToReview)
 
 module.exports = router
